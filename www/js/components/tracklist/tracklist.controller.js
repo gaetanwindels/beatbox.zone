f34ff2065f1b85948f3eb7ba0752df4c77335800
gaetanwindels <gaetan.windels@gmail.com>
2016-07-05 12:54:23 +0200

    Commit

--- www/js/components/tracklist/tracklist.controller.js
+++ www/js/components/tracklist/tracklist.controller.js
@@ -2,6 +2,8 @@ module.exports = ["TracklistService", function(TracklistService) {
 
     this.tracklistService = TracklistService;
 
+    this.tracklistService.addTrack();
+
     this.addTrack = function() {
         this.tracklistService.addTrack();
     }
--- www/js/components/waveformCursors/waveformCursors.directive.js
+++ www/js/components/waveformCursors/waveformCursors.directive.js
@@ -13,38 +13,67 @@ module.exports = function() {
 
         link: function(scope, elmt, attr) {
 
-            scope.clicked = false;
+            scope.rightClicked = false;
+            scope.leftClicked = false;
+
             scope.ngModel.left = 0;
             scope.ngModel.right = 0;
 
-            elmt.on("click", function (e) {
-                scope.clicked = true;
+            this.left = document.getElementsByClassName("wave-cursor-left")[0];
+            this.right = document.getElementsByClassName("wave-cursor-right")[0];
+
+            function detectCursor(e) {
+                scope.rightClicked = false;
+                scope.leftClicked = false;
+                var clientX = e instanceof TouchEvent ? e.touches[0].clientX : e.clientX;
+
+                if (Math.abs(clientX - left.getBoundingClientRect().right) <
+                    Math.abs(clientX - right.getBoundingClientRect().left)) {
+                    this.elemClicked = e.currentTarget.getElementsByClassName("wave-cursor-left")[0];
+                    scope.rightClicked = false;
+                    scope.leftClicked = true;
+                } else {
+                    this.elemClicked = e.currentTarget.getElementsByClassName("wave-cursor-right")[0];
+                    scope.rightClicked = true;
+                    scope.leftClicked = false;
+                }
+            }
+
+            function manageMove(e) {
                 var left = e.currentTarget.getElementsByClassName("wave-cursor-left")[0];
                 var right = e.currentTarget.getElementsByClassName("wave-cursor-right")[0];
+                var clientX = e instanceof TouchEvent ? e.touches[0].clientX : e.clientX;
 
-                if (Math.abs(e.clientX - left.getBoundingClientRect().right) <
-                    Math.abs(e.clientX - right.getBoundingClientRect().left)) {
-                    left.style.width = e.clientX + "px";
+                if (scope.leftClicked) {
+                    this.elemClicked.style.width = clientX + "px";
                     scope.$apply(function() {
-                        scope.ngModel.left = e.clientX / document.body.clientWidth;
+                        scope.ngModel.left = clientX / document.body.clientWidth;
                     });
-                } else {
-                    right.style.width = (document.body.clientWidth - e.clientX) + "px";
+                }
+                if (scope.rightClicked) {
+                    this.elemClicked.style.width = (document.body.clientWidth - clientX) + "px";
+
                     scope.$apply(function() {
                         scope.ngModel.right = 1 - right.getBoundingClientRect().left / document.body.clientWidth;
                     });
                 }
-            });
+            }
+
+            elmt.on("mousedown", detectCursor.bind(this));
+            elmt.on("touchstart", detectCursor.bind(this));
+
+            document.addEventListener("mousemove", manageMove.bind(this));
+            document.addEventListener("touchmove", manageMove.bind(this));
 
             document.addEventListener("mouseup", function (e) {
-                scope.clicked = false;
+                manageMove(e);
+                scope.rightClicked = false;
+                scope.leftClicked = false;
             });
 
-
-            document.addEventListener("mousemove", function (e) {
-                if (scope.clicked) {
-                    //e.currentTarget.getElementsByClassName("wave-cursor-left")[0].style.width = e.offsetX + "px";
-                }
+            document.addEventListener("touchend", function (e) {
+                scope.rightClicked = false;
+                scope.leftClicked = false;
             });
 
         }
